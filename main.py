dcbccaf162533a9eff50b53bd89ec45905916f45
Shyam Sundar <55319627+mayhsundar@users.noreply.github.com>
2021-10-04 18:38:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from flask import Flask
 from flask import jsonify
+import Instagram
+import random as rd
 
 app = Flask(__name__)
 
-@app.route("/h")
+@app.route("/instagram")
+def getInstaMeme():
+    posts = Instagram.InstagramCrawler()
+    postsDetails = posts.results
+    postsDetail: dict = postsDetails[0]
+    memes = []
+    for _ in postsDetail.values():
+        memes.extend(_)
+
+    meme = memes[rd.randint(0, len(memes)-1)]
+    return jsonify(meme)
+
+@app.route("/reddit")
 def getHello():
-    return jsonify({"hello":"world"})
+    return jsonify({"hello": "world"})
 
 @app.errorhandler(404)
 @app.route('/<lol>')
 def not_found(lol):
     return "<h1>Oops, I guess You are Lost?</h1>"
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
